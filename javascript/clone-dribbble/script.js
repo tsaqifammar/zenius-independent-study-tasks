58c3b925e5cddefaf133924953bd969df9df02bc
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-05-18 23:52:29 +0700

    javascript: add dark mode functionality

--- javascript/clone-dribbble/script.js
+++ javascript/clone-dribbble/script.js
@@ -1,1 +1,20 @@
-alert('hello, world!');
+
+/* ======= dark mode functionality =========== */
+
+let isInDarkMode = false;
+
+function toggleDarkMode() {
+  if (!isInDarkMode) {
+    document.body.classList.add('dark');
+    isInDarkMode = true;
+  } else {
+    document.body.classList.remove('dark');
+    isInDarkMode = false;
+  }
+}
+
+const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+console.log(toggleDarkModeButton);
+toggleDarkModeButton.addEventListener('click', toggleDarkMode);
+
+/* ============================================= */
